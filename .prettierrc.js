bbc3561d1b5a47264c7a032367e44fce1374ab2a
Dini Putri <mahardini.rizky@gmail.com>
2023-06-01 23:07:33 +1000

    tailwindcss prettier plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,6 +6,8 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: false,
-  plugins: [require.resolve('prettier-plugin-astro')],
+  plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
+  pluginSearchDirs: false,
   overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+  tailwindConfig: './tailwind.config.cjs',
 }
