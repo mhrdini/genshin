26f1104815d2d634b48485396241506b7b94a091
Dini Putri <mahardini.rizky@gmail.com>
2023-06-01 22:32:16 +1000

    project setup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   printWidth: 120,
-  semi: true,
+  semi: false,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: false,
-
   plugins: [require.resolve('prettier-plugin-astro')],
-
   overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
-};
+}
